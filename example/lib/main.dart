8c876fdd7e784ea0ad2d2f498bcca9f12768b061
librarian <57712678+LibraryLibrarian@users.noreply.github.com>
2026-09-10 20:47:07 +0900

    fix(example): 警告抑制のisolateごとの有効範囲をコメントに反映する
    Refs #13

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:misskey_emoji/misskey_emoji.dart';
 import 'app.dart';
 
 void main() {
-  // Driftのプロセス全体の診断フラグを変更し、複数サーバーのキャッシュを許可する。
+  // 現在のisolate内でDrift全体の診断フラグを変更し、複数サーバーのキャッシュを許可する。
   suppressMultipleDatabaseWarning();
   runApp(const App());
 }
